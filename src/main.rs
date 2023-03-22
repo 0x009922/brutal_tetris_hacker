67b3667f0fc271220cae9f286f9a879de5b27c05
Dmitry Balashov <a.marcius26@gmail.com>
2023-03-22 19:05:55 +0600

    a bit nicer; a bit faster; alg still sucks.

--- src/main.rs
+++ src/main.rs
@@ -159,48 +159,85 @@ mod tetro {
 }
 
 mod grid_checker {
+    use crate::grid_checker::cells::Cells;
     use grid::Grid;
-    use std::fmt::{Debug, Formatter};
 
     use crate::maths::Pos;
     use crate::tetro::{PositionedTetro, Tetro, TetroIter};
 
-    #[derive(Clone, derive_more::Display)]
-    pub enum Cell {
-        #[display(fmt = "-")]
-        Empty,
-        #[display(fmt = "x")]
-        Unavailable,
-        #[display(fmt = "+")]
-        Occupied,
-    }
+    mod cells {
+        use super::{Grid, Pos, PositionedTetro, Tetro};
 
-    impl Debug for Cell {
-        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-            std::fmt::Display::fmt(&self, f)
+        #[derive(Clone, derive_more::Display)]
+        pub enum Cell {
+            #[display(fmt = "-")]
+            Empty,
+            #[display(fmt = "x")]
+            Unavailable,
+            #[display(fmt = "+")]
+            Occupied,
         }
-    }
 
-    trait Placer {
-        fn try_put(&mut self, tetro: &Tetro) -> Option<Pos>;
+        pub struct Cells {
+            grid: Grid<Cell>,
+            initially_unavailable: usize,
+            how_many_free: usize,
+        }
 
-        fn unwind(&mut self, tetro: &PositionedTetro);
+        impl Cells {
+            pub fn new<'a>(
+                (rows, cols): (usize, usize),
+                unavailable: impl Iterator<Item = &'a (usize, usize)>,
+            ) -> Self {
+                let mut grid = Grid::init(rows, cols, Cell::Empty);
+                let mut how_many_free = grid.cols() * grid.cols();
+                let mut initially_unavailable = 0;
+                for (x, y) in unavailable {
+                    grid[*x][*y] = Cell::Unavailable;
+                    initially_unavailable += 1;
+                    how_many_free -= 1;
+                }
 
-        fn how_many_free(&self) -> usize;
-    }
+                Self {
+                    grid,
+                    how_many_free,
+                    initially_unavailable,
+                }
+            }
 
-    impl Placer for Grid<Cell> {
-        fn try_put(&mut self, tetro: &Tetro) -> Option<Pos> {
-            fn find(grid: &Grid<Cell>, tetro: &Tetro) -> Option<Pos> {
+            pub fn try_put(&mut self, tetro: &Tetro) -> Option<Pos> {
+                match self.find_any_fit_for(tetro) {
+                    Some(pos) => {
+                        for tetro_pos in tetro.iter() {
+                            let Pos { row, col } = &pos + tetro_pos;
+                            self.grid[row][col] = Cell::Occupied;
+                            self.how_many_free -= 1;
+                        }
+
+                        Some(pos)
+                    }
+                    None => None,
+                }
+            }
+
+            pub fn unwind(&mut self, PositionedTetro { tetro, pos }: &PositionedTetro) {
+                for tetro_pos in tetro.iter() {
+                    let Pos { row, col } = pos + tetro_pos;
+                    self.grid[row][col] = Cell::Empty;
+                    self.how_many_free += 1;
+                }
+            }
+
+            fn find_any_fit_for(&self, tetro: &Tetro) -> Option<Pos> {
                 let tetro_size = tetro.size();
 
-                for row in 0..(grid.rows() - tetro_size.0 + 1) {
-                    for col in 0..grid.cols() - tetro_size.1 + 1 {
+                for row in 0..(self.grid.rows() - tetro_size.0 + 1) {
+                    for col in 0..(self.grid.cols() - tetro_size.1 + 1) {
                         let pos = Pos { row, col };
 
                         let all_empty = tetro.iter().all(|tetro_pos| {
                             let Pos { row, col } = &pos + tetro_pos;
-                            matches!(grid[row][col], Cell::Empty)
+                            matches!(self.grid[row][col], Cell::Empty)
                         });
 
                         if all_empty {
@@ -212,62 +249,58 @@ mod grid_checker {
                 None
             }
 
-            match find(self, tetro) {
-                Some(pos) => {
-                    for tetro_pos in tetro.iter() {
-                        let Pos { row, col } = &pos + tetro_pos;
-                        self[row][col] = Cell::Occupied;
-                    }
-
-                    Some(pos)
-                }
-                None => None,
+            pub fn how_many_free(&self) -> &usize {
+                &self.how_many_free
             }
-        }
 
-        fn unwind(&mut self, PositionedTetro { tetro, pos }: &PositionedTetro) {
-            for tetro_pos in tetro.iter() {
-                let Pos { row, col } = pos + tetro_pos;
-                self[row][col] = Cell::Empty;
+            pub fn min_free_cells(&self) -> usize {
+                let available = self.grid.cols() * self.grid.rows() - self.initially_unavailable;
+                available % 4
             }
-        }
-
-        fn how_many_free(&self) -> usize {
-            self.iter().fold(0usize, |acc, cell| match cell {
-                Cell::Empty => acc + 1,
-                _ => acc,
-            })
         }
     }
 
     pub type Placement = Vec<PositionedTetro>;
 
-    pub fn find_placements(
-        (rows, cols): (usize, usize),
-        unavailable: impl Iterator<Item = (usize, usize)>,
+    pub fn find_placements<'a>(
+        size: (usize, usize),
+        unavailable: impl Iterator<Item = &'a (usize, usize)>,
     ) -> Vec<Placement> {
         enum RecursiveReturn {
             Cont,
             Stop,
         }
 
+        // struct FoundPlacements {
+        //     items: Vec<()>,
+        // }
+
+        struct Perf {
+            iters: u128,
+            ts: std::time::Instant,
+        }
+
         fn recursive(
-            grid: &mut Grid<Cell>,
+            cells: &mut Cells,
             stack: &mut Vec<PositionedTetro>,
             results: &mut Vec<Placement>,
+            perf: &mut Perf,
         ) -> RecursiveReturn {
-            // dbg!(&grid);
+            perf.iters += 1;
+            if perf.iters % 10_000 == 0 {
+                println!("iters: {}, time: {:.2?}", perf.iters, perf.ts.elapsed());
+            }
             for tetro in TetroIter::new() {
-                if let Some(pos) = grid.try_put(tetro) {
+                if let Some(pos) = cells.try_put(tetro) {
                     stack.push(PositionedTetro::new(tetro.clone(), pos));
-                    let ret = recursive(grid, stack, results);
+                    let ret = recursive(cells, stack, results, perf);
                     let item = stack.pop().unwrap();
-                    grid.unwind(&item);
+                    cells.unwind(&item);
                     match ret {
                         RecursiveReturn::Cont => continue,
                         RecursiveReturn::Stop => return ret,
                     }
-                } else if grid.how_many_free() == 0 {
+                } else if *cells.how_many_free() == cells.min_free_cells() {
                     results.push(stack.clone());
                     if results.len() == 1 {
                         return RecursiveReturn::Stop;
@@ -278,14 +311,19 @@ mod grid_checker {
             RecursiveReturn::Cont
         }
 
-        let mut grid = Grid::init(rows, cols, Cell::Empty);
-        for (x, y) in unavailable {
-            grid[x][y] = Cell::Unavailable;
-        }
+        let mut stack = Vec::with_capacity((size.0 * size.1) / 4);
+        let mut grid = Cells::new(size, unavailable);
 
-        let mut stack = Vec::with_capacity((grid.rows() * grid.cols()) / 4);
         let mut results = Vec::new();
-        recursive(&mut grid, &mut stack, &mut results);
+        recursive(
+            &mut grid,
+            &mut stack,
+            &mut results,
+            &mut Perf {
+                iters: 0,
+                ts: std::time::Instant::now(),
+            },
+        );
 
         results
     }
@@ -300,6 +338,7 @@ mod live_terminal_conf {
     use crossterm::{cursor, event, style::Print, terminal, ExecutableCommand, Result};
 
     use crate::grid_checker::Placement;
+    use crate::reporter::{CHAR_EMPTY, CHAR_UNAVAILABLE};
 
     struct Bounded<const N: usize, const M: usize>(usize);
 
@@ -319,12 +358,12 @@ mod live_terminal_conf {
 
     pub struct Configuration {
         pub size: (usize, usize),
-        pub occupied: HashSet<(usize, usize)>,
+        pub unavailable: HashSet<(usize, usize)>,
     }
 
     impl Configuration {
-        pub fn find_placements(self) -> Vec<Placement> {
-            super::grid_checker::find_placements(self.size, self.occupied.into_iter())
+        pub fn find_placements(&self) -> Vec<Placement> {
+            super::grid_checker::find_placements(self.size, self.unavailable.iter())
         }
     }
 
@@ -365,10 +404,10 @@ mod live_terminal_conf {
                         event::KeyCode::Char('s') => self.rows.inc(),
                         event::KeyCode::Char('a') => self.cols.dec(),
                         event::KeyCode::Char('d') => self.cols.inc(),
-                        event::KeyCode::Left => self.cursor.0.dec(),
-                        event::KeyCode::Right => self.cursor.0.inc(),
-                        event::KeyCode::Up => self.cursor.1.dec(),
-                        event::KeyCode::Down => self.cursor.1.inc(),
+                        event::KeyCode::Left => self.cursor.1.dec(),
+                        event::KeyCode::Right => self.cursor.1.inc(),
+                        event::KeyCode::Up => self.cursor.0.dec(),
+                        event::KeyCode::Down => self.cursor.0.inc(),
                         event::KeyCode::Char(' ') => self.toggle_under_cursor(),
                         _ => {}
                     }
@@ -394,14 +433,11 @@ mod live_terminal_conf {
         pub fn into_configuration(self) -> Configuration {
             Configuration {
                 size: (self.rows.0, self.cols.0),
-                occupied: self.occupied,
+                unavailable: self.occupied,
             }
         }
 
         fn print(&self) -> Result<()> {
-            const FIELD_CHAR_EMPTY: char = '-';
-            const FIELD_CHAR_SET: char = '+';
-
             stdout()
                 .execute(cursor::MoveTo(0, 0))?
                 .execute(Print(
@@ -416,10 +452,10 @@ mod live_terminal_conf {
             for row in 0..self.rows.0 {
                 stdout().execute(Print("  "))?;
                 for col in 0..self.cols.0 {
-                    let ch = if self.occupied.contains(&(col, row)) {
-                        FIELD_CHAR_SET
+                    let ch = if self.occupied.contains(&(row, col)) {
+                        CHAR_UNAVAILABLE
                     } else {
-                        FIELD_CHAR_EMPTY
+                        CHAR_EMPTY
                     };
                     stdout().execute(Print(ch))?;
                 }
@@ -427,8 +463,8 @@ mod live_terminal_conf {
             }
 
             stdout().execute(cursor::MoveTo(
-                (cursor_base.0 + self.cursor.0 .0) as u16,
-                (cursor_base.1 + self.cursor.1 .0) as u16,
+                (cursor_base.0 + self.cursor.1 .0) as u16,
+                (cursor_base.1 + self.cursor.0 .0) as u16,
             ))?;
 
             Ok(())
@@ -455,73 +491,132 @@ mod reporter {
 
     use std::io::stdout;
 
+    use crossterm::style::{Attribute, Color, ResetColor, SetAttribute, SetForegroundColor};
     use crossterm::{style::Print, ExecutableCommand, Result};
     use grid::Grid;
 
     use crate::grid_checker::Placement;
+    use crate::live_terminal_conf::Configuration;
     use crate::maths::Pos;
     use crate::tetro::PositionedTetro;
 
-    pub trait Report {
-        fn report(&self, grid_size: &(usize, usize)) -> Result<()>;
-    }
+    pub const CHAR_EMPTY: char = '·';
+    pub const CHAR_UNAVAILABLE: char = '×';
 
-    impl Report for Placement {
-        fn report(&self, (rows, cols): &(usize, usize)) -> Result<()> {
-            #[derive(Clone)]
-            struct View {
-                char: char,
+    pub fn report(placement: &Placement, conf: &Configuration) -> Result<()> {
+        #[derive(Clone)]
+        struct View {
+            char: char,
+            color: Option<Color>,
+            attrs: Vec<Option<Attribute>>,
+        }
+
+        struct ViewsComposer {
+            views: HashMap<PositionedTetro, View>,
+            colors: Vec<Color>,
+            attrs: Vec<Option<Attribute>>,
+        }
+
+        impl ViewsComposer {
+            fn new() -> Self {
+                Self {
+                    views: HashMap::new(),
+                    colors: vec![
+                        Color::Green,
+                        Color::Cyan,
+                        Color::Blue,
+                        Color::Magenta,
+                        Color::Yellow,
+                    ],
+                    attrs: vec![None, Some(Attribute::Bold), Some(Attribute::Italic)],
+                }
             }
 
-            let views = self.iter().fold(HashMap::new(), |mut acc, item| {
-                acc.insert(
-                    item.clone(),
+            fn update(mut self, item: PositionedTetro) -> Self {
+                let idx = self.views.len();
+
+                let sym = char::from_u32(
+                    // ASCII 'a'
+                    (97 + idx) as u32,
+                )
+                .unwrap();
+
+                let idx_in_colors_total = idx % (self.colors.len() * self.attrs.len());
+                let idx_in_colors = idx_in_colors_total % self.colors.len();
+                let idx_in_attrs = idx_in_colors_total / self.colors.len();
+
+                self.views.insert(
+                    item,
                     View {
-                        char: char::from_u32(
-                            // ASCII 'a'
-                            (97 + acc.len()) as u32,
-                        )
-                        .unwrap(),
+                        char: sym,
+                        color: Some(self.colors[idx_in_colors]),
+                        attrs: vec![self.attrs[idx_in_attrs]],
                     },
                 );
-                acc
-            });
-
-            let mut grid = Grid::init(*rows, *cols, View { char: '-' });
-
-            for item in self {
-                let PositionedTetro { pos, tetro } = item;
-                let view = views.get(item).unwrap();
-                for i_pos in tetro.iter() {
-                    let Pos { row, col } = pos + i_pos;
-                    grid[row][col] = (*view).clone();
-                }
+                self
             }
-
-            for row in 0..grid.rows() {
-                stdout().execute(Print("  "))?;
-                for x in grid.iter_row(row) {
-                    stdout().execute(Print(x.char))?;
-                }
-                stdout().execute(Print("\n"))?;
-            }
-
-            Ok(())
         }
+
+        let ViewsComposer { views, .. } = placement
+            .iter()
+            .fold(ViewsComposer::new(), |acc, item| acc.update(item.clone()));
+
+        let mut grid = Grid::init(
+            conf.size.0,
+            conf.size.1,
+            View {
+                char: CHAR_EMPTY,
+                color: None,
+                attrs: vec![Some(Attribute::Dim)],
+            },
+        );
+
+        for item in placement {
+            let PositionedTetro { pos, tetro } = item;
+            let view = views.get(item).unwrap();
+            for i_pos in tetro.iter() {
+                let Pos { row, col } = pos + i_pos;
+                grid[row][col] = (*view).clone();
+            }
+        }
+
+        for (row, col) in conf.unavailable.iter() {
+            grid[*row][*col] = View {
+                char: CHAR_UNAVAILABLE,
+                color: Some(Color::Red),
+                attrs: Vec::new(),
+            }
+        }
+
+        for row in 0..grid.rows() {
+            stdout().execute(Print("  "))?;
+            for x in grid.iter_row(row) {
+                if let Some(col) = x.color {
+                    stdout().execute(SetForegroundColor(col))?;
+                }
+                stdout().execute(Print(x.char))?;
+                for attr in x.attrs.iter().flatten() {
+                    stdout().execute(SetAttribute(*attr))?;
+                }
+                stdout().execute(ResetColor)?;
+            }
+            stdout().execute(Print("\n"))?;
+        }
+
+        Ok(())
     }
 }
 
 fn main() -> Result<()> {
-    let conf = live_terminal_conf::State::new(4, 4)
+    let conf = live_terminal_conf::State::new(6, 6)
         .configure()?
         .into_configuration();
-    let size = conf.size;
     let placements = conf.find_placements();
 
     stdout().execute(Print(format!("Found placements: {}\n\n", placements.len())))?;
 
     for item in placements {
-        reporter::Report::report(&item, &size)?;
+        reporter::report(&item, &conf)?;
         stdout().execute(Print("\n"))?;
     }
 
